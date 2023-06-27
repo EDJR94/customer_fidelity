a343e16152e02d9a35e54f5e9b9e6a6693c95a6f
EDJR94 <edilson07.santos1@gmail.com>
2023-06-27 07:36:55 -0300

    Starting Pipeline

--- src/run_model.sh
+++ src/run_model.sh
@@ -5,4 +5,4 @@ data=$(date +'%Y-%m-%dT%H:%M:%S')
 path='C:/Users/edils/repos/customer_fidelity'
 path_to_envs='C:/users/edils/anaconda3/envs/customer_fidelity/lib/site-packages'
 
-$path_to_envs/papermill $path/src/models/v15_customer_fidelity_program_deploy_clean.ipynb $path/reports/v15_customer_fidelity_program_deploy_clean_$data.ipynb
+$path_to_envs/papermill/__init__.py $path/src/models/v15_customer_fidelity_program_deploy_clean.ipynb $path/reports/v15_customer_fidelity_program_deploy_clean_$data.ipynb
